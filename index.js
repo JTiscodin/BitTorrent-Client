aba99ef3de9aedacc8404dcbe105b95831a97528
SKukreti0107 <shubhamkukreti.0107@gmail.com>
2024-11-21 17:39:35 +0530

    restructure index.js and add download.js

--- index.js
+++ index.js
@@ -6,9 +6,13 @@ import { Buffer } from "buffer";
 
 import { getPeers } from "./src/tracker";
 
-import torrentParser from "./src/torrent-parser"
+// import torrentParser from "./src/torrent-parser"
 
-const torrent = torrentParser.open("./gtav.torrent")
+// const torrent = torrentParser.open("./gtav.torrent")
+// added for download - Shubh
+const download = require('./src/downlaod');
+const torrentParser = require('./src/torrent-parser');
+const torrent = torrentParser.open(process.argv[2]);
 
 getPeers(torrent, peers => {
     console.log("list of peers", peers)
