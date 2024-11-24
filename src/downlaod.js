ab19784e93417204aad98cdd7d16d175765a236c
SKukreti0107 <shubhamkukreti.0107@gmail.com>
2024-11-24 17:55:00 +0530

    added message grouping -onWholeMSg

--- src/downlaod.js
+++ src/downlaod.js
@@ -1,6 +1,7 @@
 const net = require('net');
 const buffer = require('buffer').Buffer;
 const tracker = require('./tracker');
+const { on } = require('events');
 
 module.exports = torrent => {
     tracker.getPeers(torrent, peers => {
@@ -14,7 +15,25 @@ function downlaod(peer) {
     socket.connect(peer.port, peer.ip, () => {
         // socket.write()...
     });
-    socket.on('data', data => {
-        // handle response
+    onWholeMsg(socket, data =>{
+        //handle response
+    });
+}
+
+function onWholeMsg(socket, callback) {
+    let savedBuf = buffer.alloc(0);
+    let handshake = true;
+
+    socket.on('data', recvBuf => {
+        //msgLen calculates the length of the message
+        const msgLen = () => handshake ? savedBuf.readUInt8(0) + 49 : savedBuf.readInt32BE(0) + 4;
+        savedBuf = Buffer.concat([savedBuf, recvBuf]); 
+
+        while(savedBuf.length >= 4 && savedBuf.length >= msgLen()){
+
+            callback(savedBuf.slice(0,msgLen()));
+            savedBuf = savedBuf.slice(msgLen());
+            handshake = false;
+        }
     });
 }
